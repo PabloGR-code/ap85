9b4f3b0d81234609527fd3287dd47e79070fd52f
Pablo <pagira@alumnatflorida.es>
2026-03-03 12:00:22 +0100

    añadiendo cambios

--- AP63v3/models/GestorPdo.php
+++ AP63v3/models/GestorPdo.php
@@ -19,12 +19,30 @@ class GestorPdo extends Connection{
     }
 
     public function agregar(Bici $p){
-        $nombre=$p->getNombre();
-        $precio=$p->getPrecio();
-        $electrica=$p->getElectrica();
-        $consulta="INSERT INTO bici (nombre, precio, electrica) VALUES ($nombre, $precio, $electrica)";
-        $stmt=$this->conn->exec($consulta); //se pone exec con insert, update, delete
+
+        $consulta = "INSERT INTO bici (nombre, precio, electrica) VALUES (:nombre, :precio, :electrica)";
+
+        $stmt = $this->conn->prepare($consulta);
+
+        $stmt->bindValue(':nombre', $p->getNombre());
+        $stmt->bindValue(':precio', $p->getPrecio());
+        $stmt->bindValue(':electrica', $p->getElectrica());
+
+        $stmt->execute();
+
         return $stmt;
+    }
+
+    public function eliminar($id){
+        $consulta = "DELETE FROM bici WHERE id=:id";
+        $stmt=$this->conn->prepare($consulta);
+        $stmt->bindValue(':id', $id);
+        $stmt ->execute();
+        return $stmt;
+    }
+
+    public function editar(){
+        
     }
 
 }
